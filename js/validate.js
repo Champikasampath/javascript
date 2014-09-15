c96191d5e4e383842b4ceec2e63f5224a9f5e3a9
champika <champika.sampath@abstraction.co>
2014-09-15 12:56:40 +0530

    testjs add

---
+++ js/validate.js
@@ -0,0 +1,32 @@
+ function init()
+ {
+     'use strict';
+    if(document && document.getElementById)
+    {
+        var loginForm= document.getElementById("loginForm");
+        loginForm.onsubmit= validateForm;
+
+    }
+ }
+
+
+ /**
+  * function validate data of login form
+  * @returns {boolean}
+  */
+ function validateForm()
+ {
+    'use strict';
+    var email=document.getElementById("email");
+    var password=document.getElementById("pw");
+
+    if((email.value.length>0) && (email.value.length>0))
+    {
+        return true;
+    }
+    else
+    {
+        alert("please complete the form");
+        return false;
+    }
+ }
